d8e47930bb17b185ed1987dffd365fbfc26c2a13
MoNakouzi <mo.nakouzi03@gmail.com>
2024-03-02 13:31:21 -0500

    Initial setup for website

---
+++ src/app/_components/navbar.jsx
@@ -0,0 +1,38 @@
+export default function Navbar() {
+    return (
+        <>
+            <section id="navbar" className="bg-background h-16 top-0 w-full">
+                <nav className="hidden md:flex main">
+                    <div className="flex items-center justify-between w-full mx-auto h-full">
+                        <div className="flex items-center font-serif w-full h-full">
+                            <a href="#home" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                Home
+                            </a>
+                            <a href="#about" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                About
+                            </a>
+                            <a href="#schedule" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                Schedule
+                            </a>
+                            <a href="#speakers" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                Speakers
+                            </a>
+                            <a href="#sponsors" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                Sponsors
+                            </a>
+                            <a href="#contact" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                Contact
+                            </a>
+                        </div>
+                        <div className="flex items-center space-x-4 h-full font-serif">
+                            <a href="#rsvp" className="flex items-center text-foreground px-4 lg:px-6 font-bold h-full hover:bg-highlight-dark hover:text-background ease-in-out duration-200">
+                                RSVP
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </section>
+            <div className='h-16'></div>
+        </>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      "background": "#F9F8FD",
+      "foreground": "#191F2D",
+      "highlight": "#CBC5EA",
+      "highlight-light": "#E5E2F5",
+      "highlight-dark": "#525072"
+    },
+    fontFamily: {
+      sans: ['var(--sans)'],
+      serif: ['var(--serif)'],
     },
   },
   plugins: [],
