41393ff7b9aaa9983521294d08772fd2a582f2ae
MoNakouzi <mo.nakouzi03@gmail.com>
2024-03-08 11:44:20 -0500

    creating agenda

---
+++ src/app/_components/agenda.jsx
@@ -0,0 +1,7 @@
+export default function Agenda() {
+    return (
+        <section id="agenda" className="lg:text-lg">
+            <h1>Agenda</h1>
+        </section>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Intro from '@/app/_components/intro';
 import About from '@/app/_components/about';
+import Agenda from '@/app/_components/agenda';
 
 export default function Home() {
   return (
     <>
       <Intro />
       <About />
+      <Agenda />
     </>
   );
 }
