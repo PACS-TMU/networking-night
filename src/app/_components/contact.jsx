bb2e8aa03a86f268ed64982c706c30ab12471cf5
monakouzi <mo.nakouzi03@gmail.com>
2024-03-09 16:22:27 -0500

    creating contact us section

---
+++ src/app/_components/contact.jsx
@@ -0,0 +1,22 @@
+export default function Contact() {
+    return (
+        <section id="contact" className="lg:text-lg">
+            <div className="main">
+                <h1 className='text-foreground text-3xl pb-8 md:text-4xl lg:text-5xl font-serif tracking-wide leading-relaxed font-semibold'>
+                    Contact Us
+                </h1>
+                <div className="flex flex-col justify-center text-foreground font-sans w-full lg:text-lg">
+                    <p className="text-foreground">
+                        PACS and USSTM are dedicated to helping students learn more about the tech industry and to help them make connections within the industry.
+                        If you have any questions about the event, please feel free to reach out to us 
+                        at <a href="mailto:pacs@usstm.ca" className="text-highlight-dark underline">pacs@usstm.ca</a>. For updates about the event,
+                        please follow us on instagram 
+                        at <a href="https://www.instagram.com/pacs_tmu/" target="_blank" className="text-highlight-dark underline">@pacs_tmu</a>. To RSVP 
+                        for the event, please visit 
+                        our <a href="#rsvp" target="_blank" className="text-highlight-dark underline">RSVP section</a>.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Intro from '@/app/_components/intro';
 import About from '@/app/_components/about';
 import Agenda from '@/app/_components/agenda';
 import Panelists from './_components/panelists';
+import Contact from './_components/contact';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Agenda />
       <Panelists />
+      <Contact />
     </>
   );
 }
